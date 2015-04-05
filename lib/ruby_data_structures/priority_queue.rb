cfbdb506940c14d5ae6022983da8df24cb508750
ichthala <alice.mottola@gmail.com>
2015-04-04 20:27:27 -0400

    Change specs

--- lib/ruby_data_structures/priority_queue.rb
+++ lib/ruby_data_structures/priority_queue.rb
@@ -1,6 +1,28 @@
 class PriorityQueue
 
-  def initialize
+  def initialize events={}
+    raise ArgumentError, 'Events must be passed to the priority queue as a hash' unless events.is_a?(Hash)
+
+    @queue = []
+    events.each { |k,v| insert(k, v)}
   end
+
+  def insert k, v
+    # event = Struct.new :k, :v
+    # event.k = k.to_i
+    # event.v = v
+
+    # queue << event
+    # index = queue[-1]
+  end
+
+
+  def min
+    queue[0]
+  end
+
+  private
+
+  attr_reader :queue
 
 end
--- spec/ruby_data_structures/priority_queue_spec.rb
+++ spec/ruby_data_structures/priority_queue_spec.rb
@@ -2,8 +2,34 @@ require 'spec_helper'
 
 describe PriorityQueue do
 
-  it 'is a real thing' do
-    expect(PriorityQueue.new.is_a?(PriorityQueue)).to eq true
+  describe '::new' do
+
+    it 'inserts events' do
+      events = {
+        '8' => 'Tennis Court Oath',
+        '10' => 'Bastille Day',
+        '18' => 'Reign of Terror'
+      }
+
+      expect_any_instance_of(PriorityQueue).to receive(:insert).with('8', 'Tennis Court Oath')
+      expect_any_instance_of(PriorityQueue).to receive(:insert).with('10', 'Bastille Day')
+      expect_any_instance_of(PriorityQueue).to receive(:insert).with('18', 'Reign of Terror')
+
+      PriorityQueue.new events
+    end
+
+    context 'when a non-Hash argument is passed' do
+      it 'raises an ArgumentError' do
+        expect { PriorityQueue.new(['l', 'o', 'l']) }.to raise_error
+      end
+    end
+
+    context 'when no arguments are passed' do
+      it 'does not raise an error' do
+        expect { PriorityQueue.new }.not_to raise_error
+      end
+    end
+
   end
 
 end
